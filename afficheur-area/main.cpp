11365843eee6017f9038cadde3eb35452e495b18
tvaira <tvaira@free.fr>
2022-03-14 12:29:55 +0100

    AERAPI22-7 Initialise le projet Qt

---
+++ afficheur-area/main.cpp
@@ -0,0 +1,26 @@
+#include "ihmafficheur.h"
+#include <QApplication>
+
+/**
+ * @file main.cpp
+ * @brief Programme principal
+ * @details Crée et affiche la fenêtre principale de l'application
+ * Afficheur-AREA
+ * @author
+ * @version 1.0
+ *
+ * @param argc
+ * @param argv[]
+ * @return int
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    IHMAfficheur w;
+
+    w.show();
+
+    return a.exec();
+}
